52bf72ae248ea584d3700c8e9d17f09580cf2ca1
ravi9409 <39340361+ravi9409@users.noreply.github.com>
2026-06-07 19:36:10 +0530

    main

---
+++ src/synchronize/Counter.java
@@ -0,0 +1,14 @@
+package synchronize;
+
+public class Counter {
+    
+    private int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public synchronized int getCount() {
+        return count;
+    }
+}
---
+++ src/synchronize/Test.java
@@ -0,0 +1,21 @@
+package synchronize;
+
+public class Test {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        MyThread t1 = new MyThread(counter);
+        MyThread t2 = new MyThread(counter);
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final count: " + counter.getCount());
+    }
+}
